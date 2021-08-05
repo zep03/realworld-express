7210e2e69498f1e06e66e4ba6ecb5fc37052d78d
zep03 <910456393@qq.com>
2021-08-05 19:00:27 +0800

    '提取layout'

--- router/index.js
+++ router/index.js
@@ -16,10 +16,29 @@ router.get('/register', (req, res) => {
     res.render('login.html')
 })
 
-router.get('/profile', (req, res) => {
+router.get('/settings', (req, res) => {
+    res.render('settings.html')
+})
+
+router.get('/editor', (req, res) => {
+    res.render('editor.html')
+})
+
+router.get('/editor:articleId', (req, res) => {
+    res.render('editor.html')
+})
+
+router.get('/article/:articleId', (req, res) => {
+    res.render('article.html')
+})
+
+router.get('/profile/:username', (req, res) => {
     res.render('profile.html')
 })
 
+router.get('/profile/:username/favorites', (req, res) => {
+    res.render('profile.html')
+})
 
 
 module.exports = router
